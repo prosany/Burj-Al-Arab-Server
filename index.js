c4f1db23c5fc609ceff1dd4275a1cbf441f96db9
Mahabub Sunny <office.prosany@gmail.com>
2021-03-29 00:49:39 +0600

    Remove Sencitive Informations

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ admin.initializeApp({
 });
 
 // Mongodb
-const password = 'Sunny[564696]';
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hgubw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
